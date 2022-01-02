49d11810a2cc13e6f0c11245f30afd3edc4e6d9a
sixunity <68563790+sixunity@users.noreply.github.com>
2022-01-02 17:38:12 +0200

    Update sisolas.js

--- sarunas/sisolas.js
+++ sarunas/sisolas.js
@@ -1,5 +1,5 @@
-const {abzp} = require(`./atbilzhandleris.js`)
-const divainaszivsarray = [`Ļoti smieklīgi`,`Kāds mums te jokdaris atradies`,`Eu paskaties <@594176832023298048>`,`Eu paskaties <@594176832023298048>`,`Eu paskaties <@594176832023298048>`,`Eu paskaties <@594176832023298048>`,`Eu paskaties <@594176832023298048>`,`Eu paskaties <@594176832023298048>`,`Eu paskaties <@594176832023298048>`,`Eu paskaties <@594176832023298048>`];
+const {abzp, randoms} = require(`./atbilzhandleris.js`)
+const arajs1 = [`Ļoti smieklīgi`,`Kāds mums te jokdaris atradies`,`Eu paskaties <@594176832023298048>`,`Eu paskaties <@594176832023298048>`,`Eu paskaties <@594176832023298048>`,`Eu paskaties <@594176832023298048>`,`Eu paskaties <@594176832023298048>`,`Eu paskaties <@594176832023298048>`,`Eu paskaties <@594176832023298048>`,`Eu paskaties <@594176832023298048>`];
               const divaina_ = [`Šīs`,`šīs`,`sis`,`Sis`,`šīm`,`Šīm`];
               const _zivs = [`Olas`,`olas`, `olām`,`Olām`];
         
@@ -7,7 +7,7 @@ module.exports = (client, Discord, message) => {
 	
     
     if (abzp(message,divaina_,_zivs)){
-      message.channel.send(randoms(divainaszivsarray));
+      message.channel.send(randoms(arajs1));
     //  const divzivs = client.emojis.cache.find(emoji => emoji.name === "DivainaZivs");
 	}
   return;
